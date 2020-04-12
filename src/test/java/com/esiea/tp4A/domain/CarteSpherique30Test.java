e2722926f1a45f6684a700870a8499b3db4f6e5a
isbappa <isbappa@gmail.com>
2020-04-12 23:15:10 +0200

    adding MoveForwardBackward Test

--- src/test/java/com/esiea/tp4A/domain/CarteSpherique30Test.java
+++ src/test/java/com/esiea/tp4A/domain/CarteSpherique30Test.java
@@ -44,28 +44,7 @@ class CarteSpherique30Test {
         //Assertions.assertThat(newPosition).isEqualTo(Position.of(expectedX, expectedY, expectedDirection));
         Assertions.assertThat(newPosition).isEqualToComparingFieldByField(Position.of(expectedX, expectedY, expectedDirection));
     }
-    @ParameterizedTest
-    @CsvSource({
-        "b, 0, -1, NORTH",
-        "l, 0, 0, WEST",
-        "r, 0, 0, EAST",
-        "ff, 0, 2, NORTH",
-        "lf, -1, 0, WEST",
-        "rf, 1, 0, EAST",
-        "llf, 0, -1, SOUTH",
-        "bb, 0, -2, NORTH",
-        "lb, 1, 0, WEST",
-        "llb, 0, 1, SOUTH",
-        "rb, -1, 0, EAST",
-        "fflb, 1, 2, WEST"
-    })
-    void rover_moves_forward_backward(String command, int expectedX, int expectedY, Direction expectedDirection) {
-        MarsRover marsRover = new MarsRoverImpl(0, 0, Direction.NORTH, map);
 
-        Position newPosition = marsRover.move(command);
-        //Assertions.assertThat(newPosition).isEqualTo(Position.of(expectedX, expectedY, expectedDirection));
-        Assertions.assertThat(newPosition).isEqualToComparingFieldByField(Position.of(expectedX, expectedY, expectedDirection));
-    }
     @ParameterizedTest
     @CsvSource({
         "lf, 25, 0, WEST",
