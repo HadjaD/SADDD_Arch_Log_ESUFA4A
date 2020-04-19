c85db0c83ed0fa0569c89b1eaa9a589a4e80de4c
isbappa <isbappa@gmail.com>
2020-04-20 00:57:28 +0200

    fixing bug due to recent commit

--- src/test/java/com/esiea/tp4A/RequestHandlerTest.java
+++ src/test/java/com/esiea/tp4A/RequestHandlerTest.java
@@ -24,49 +24,6 @@ public class RequestHandlerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @Test
-    public void indexReturn() throws Exception{
-        this.mockMvc.perform(get("/")).andDo(print())
-            .andExpect(status().isOk())
-            .andExpect(content().string(containsString("Welcome to Mars Rover : let's start your game !\n" +
-                "Commands :\n" +
-                "Create Game : post > /api/session/{gameName}\n" +
-                "Create Player : post > /api/session/{gameName}/player/{playerName}\n" +
-                "Get Player : get > /api/session/{gameName}/player/{playerName}\n" +
-                "Command Player : patch > /api/session/{gameName}/player/{playerName}/{command}")));
-    }
-
-    @Test
-    public void createGame() throws Exception {
-        this.mockMvc.perform(post("/api/session/one"))
-            .andExpect(status().isOk())
-            .andExpect(content().contentType("application/json"))
-            .andExpect(jsonPath("name", Matchers.is("one")));;
-    }
-
-    @Test
-    public void createExistingGame() throws Exception {
-        this.mockMvc.perform(post("/api/session/second"));
-        this.mockMvc.perform(post("/api/session/second"))
-            .andExpect(status().isConflict());
-    }
-
-    @Test
-    public void createPlayer() throws Exception {
-        this.mockMvc.perform(post("/api/session/third"));
-        this.mockMvc.perform(post("/api/session/third/player/rover"))
-            .andExpect(status().isOk())
-            .andExpect(content().contentType("application/json"))
-            .andExpect(jsonPath("player.name", Matchers.is("rover")));
-    }
-
-    @Test
-    public void createExistingPlayer() throws Exception {
-        this.mockMvc.perform(post("/api/session/fourth"));
-        this.mockMvc.perform(post("/api/session/fourth/player/rover"));
-        this.mockMvc.perform(post("/api/session/fourth/player/rover"))
-            .andExpect(status().isConflict());
-    }
 
     @Test
     public void createPlayerOnUnknownGame() throws Exception {
@@ -74,15 +31,6 @@ public class RequestHandlerTest {
             .andExpect(status().isNotFound());
     }
 
-    @Test
-    public void GetExistingPlayer() throws Exception{
-        this.mockMvc.perform(post("/api/session/fifth"));
-        this.mockMvc.perform(post("/api/session/fifth/player/rover"));
-        this.mockMvc.perform(get("/api/session/fifth/player/rover"))
-            .andExpect(status().isOk())
-            .andExpect(content().contentType("application/json"))
-            .andExpect(jsonPath("player.name", Matchers.is("rover")));;
-    }
 
     @Test
     public void GetUnknownPlayer() throws Exception{
@@ -91,13 +39,6 @@ public class RequestHandlerTest {
             .andExpect(status().isNotFound());
     }
 
-    @Test
-    public void movePlayer() throws Exception{
-        this.mockMvc.perform(post("/api/session/seventh"));
-        this.mockMvc.perform(post("/api/session/seventh/player/rover"));
-        this.mockMvc.perform(patch("/api/session/seventh/player/rover/l"))
-            .andExpect(status().isOk());
-    }
 
     @Test
     public void moveUnknownPlayer() throws Exception{
@@ -106,13 +47,6 @@ public class RequestHandlerTest {
             .andExpect(status().isNotFound());
     }
 
-    @Test
-    public void movePlayerWithUnknownCommand() throws Exception{
-        this.mockMvc.perform(post("/api/session/ninth"));
-        this.mockMvc.perform(post("/api/session/ninth/player/rover"));
-        this.mockMvc.perform(patch("/api/session/ninth/player/rover/a"))
-            .andExpect(status().isBadRequest());
-    }
 
 
 }
